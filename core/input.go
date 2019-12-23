b1b2493f9f6ebefe26d5af1258aceabeea730fc6
pedritoelcabra <pedrito_elcabra@hotmail.com>
2019-12-23 12:41:31 +0100

    Move input setup function

--- core/input.go
+++ core/input.go
@@ -1,6 +1,9 @@
 package core
 
-import "github.com/hajimehoshi/ebiten"
+import (
+	"github.com/hajimehoshi/ebiten"
+	"github.com/pedritoelcabra/projectx/world"
+)
 
 type listenerFunction func(g *game)
 
@@ -85,4 +88,65 @@ func (i *Input) TriggerCallbacks(key string) {
 	for _, callback := range i.listeners[key] {
 		callback(projectX)
 	}
+}
+
+func (g *game) InitInput() {
+	g.Input = NewInput()
+	g.Input.AddListener("RightClick", "openContext", func(g *game) {
+		g.Gui.AddMenu("context", g.BuildContextMenu(ebiten.CursorPosition()))
+	})
+	g.Input.AddListener("LeftClick", "closeContext", func(g *game) {
+		g.Gui.SetDisabled("context", true)
+	})
+	g.Input.AddListener("EscapePress", "toggleMenu", func(g *game) {
+		g.TogglePause()
+	})
+	g.Input.AddListener("LeftPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERLEFT, true)
+	})
+	g.Input.AddListener("LeftRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERLEFT, false)
+	})
+	g.Input.AddListener("APress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERLEFT, true)
+	})
+	g.Input.AddListener("ARelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERLEFT, false)
+	})
+	g.Input.AddListener("RightPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERRIGHT, true)
+	})
+	g.Input.AddListener("RightRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERRIGHT, false)
+	})
+	g.Input.AddListener("DPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERRIGHT, true)
+	})
+	g.Input.AddListener("DRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERRIGHT, false)
+	})
+	g.Input.AddListener("UpPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERUP, true)
+	})
+	g.Input.AddListener("UpRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERUP, false)
+	})
+	g.Input.AddListener("WPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERUP, true)
+	})
+	g.Input.AddListener("WRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERUP, false)
+	})
+	g.Input.AddListener("DownPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERDOWN, true)
+	})
+	g.Input.AddListener("DownRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERDOWN, false)
+	})
+	g.Input.AddListener("SPress", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERDOWN, true)
+	})
+	g.Input.AddListener("SRelease", "updatePlayer", func(g *game) {
+		g.UpdatePlayerMovement(world.PLAYERDOWN, false)
+	})
 }
